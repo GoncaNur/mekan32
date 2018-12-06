d9d08a91832cd5b5f7d51497a7c8b7d5a468b785
Asım Sinan Yüksel <asimsinanyuksel@erkans-MacBook-Pro.local>
2018-12-06 15:01:44 +0300

    7. ders

--- app.js
+++ app.js
@@ -2,20 +2,21 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var favicon = require('serve-favicon');
+
+require('./app_api/models/db');
+
+var routesApi = require('./app_api/routes/index'); 
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
-require('./app_server/models/db'); 
-
+ 
 var app = express();
-app.use(favicon(path.join(__dirname, 'public', 'images','favicon.ico')))
+app.use('/api', routesApi);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server','views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
